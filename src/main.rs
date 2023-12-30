c5dc34d1f01d74d9a9cdac872d18d68bdc7986dd
AthithyanR <athithyanr4@gmail.com>
2023-12-30 21:35:11 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use axum::{
+    http::{HeaderMap, Uri},
+    response::{Html, IntoResponse},
+    serve, Router,
+};
+use std::{env, fs};
+use tokio::net;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().fallback(handler);
+    let listener = net::TcpListener::bind("0.0.0.0:3000").await?;
+    serve(listener, app).await?;
+    Ok(())
+}
+
+async fn handler(uri: Uri) -> impl IntoResponse {
+    let req_path = &uri.to_string()[1..]; // removing the / from beginning to make it a non-absolute path
+
+    let mut target_path = env::current_dir().expect("Cannot get the current directory!");
+    target_path.push(req_path);
+
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "text/html".parse().unwrap());
+
+    if !target_path.exists() {
+        return Html("<html><body>Unable to access the specified path</body></html>".to_string());
+    }
+
+    let metadata = fs::metadata(&target_path).unwrap();
+    if metadata.is_dir() {
+        let mut response = String::new();
+        response.push_str("<html><body><h1>Directory listing for /</h1><hr><ul>");
+
+        for entry in fs::read_dir(&target_path).unwrap() {
+            let entry = entry.unwrap();
+            let file_name = entry.file_name();    
+            let file_name = &file_name.to_string_lossy()[0..file_name.len()];
+
+            response.push_str(
+                &format!(
+                    "<li>
+                        <a href='/{}'>{}</a>
+                    </li>",
+                    file_name,
+                    file_name,
+                )
+            )
+        }
+
+        response.push_str("</ul><hr></body></html>");
+        return Html(response);
+    }
+
+   Html("<html><body>Is a file</body></html>".to_string())
 }
